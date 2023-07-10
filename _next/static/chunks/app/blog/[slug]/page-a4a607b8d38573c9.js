87fd0b2055a9583e81e114b9508f2b51e3f96f21
Mohamed Moughamir <moughamir@gmail.com>
2023-07-10 17:54:13 +0100

    Deploy

---
+++ _next/static/chunks/app/blog/[slug]/page-a4a607b8d38573c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308,301],{86:function(){}},function(n){n.O(0,[774,597,744],function(){return n(n.s=86)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/page-6f0e31c8c3ab725c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7937:function(n,e,u){Promise.resolve().then(u.t.bind(u,486,23))}},function(n){n.O(0,[486,774,597,744],function(){return n(n.s=7937)}),_N_E=n.O()}]);
