cbdb8c5f2f00704203bc324d3d0d221ce8cc71f8
InteliSynth <moughamir@gmail.co>
2025-01-08 16:20:43 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,9 @@
 import { NextConfig } from 'next';
-import withTM from 'next-transpile-modules';
+
 
 
 const nextConfig: NextConfig = {
   output: 'export',
-  experimental: {
-    turbo: {
-      rules: {
-        '*.glsl': {
-          loaders: ['raw-loader', 'glslify-loader'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 };
 
-export default withTM(['three'])(nextConfig);
+export default nextConfig;
